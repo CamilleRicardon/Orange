38b843d34477b59eaaac54ee5d212f2db8f5f8a9
Camille <camille.ricardon@epitech.eu>
2023-07-17 15:42:09 +0200

    Now working DB. Don't worry to launch the database

--- dist/app.js
+++ dist/app.js
@@ -6,13 +6,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const person_1 = require("./person");
-const person_2 = require("./person");
 const cors_1 = __importDefault(require("cors"));
 const fs_1 = __importDefault(require("fs"));
+const mysql2_1 = __importDefault(require("mysql2"));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(body_parser_1.default.json());
 const port = 3000;
+//const mysql = require('mysql2');
 function readDataFromFile() {
     try {
         const data = fs_1.default.readFileSync('dist/data.json', 'utf8');
@@ -23,24 +24,52 @@ function readDataFromFile() {
         return [];
     }
 }
+const connection = mysql2_1.default.createConnection({
+    host: 'localhost',
+    user: 'Camille',
+    password: 'linux_06',
+    database: 'my_db'
+});
 let people = readDataFromFile();
+connection.connect((error) => {
+    if (error) {
+        console.error('Erreur de connexion à la base de données :', error);
+    }
+    else {
+        console.log('Connexion à la base de données réussie');
+    }
+});
 app.get('/people', (req, res) => {
-    res.json(people);
+    const query = 'SELECT * FROM personnes';
+    connection.query(query, (error, results) => {
+        if (error) {
+            console.error('Erreur lors de la récupération des personnes :', error);
+            res.status(500).json({ error: 'Erreur serveur' });
+        }
+        else {
+            res.json(results);
+        }
+    });
 });
 app.get('/people/:id', (req, res) => {
     const id = req.params.id;
-    const person = people.find(person => person.id === id);
-    if (person) {
-        res.json(person);
-    }
-    else {
-        res.status(404).json({ error: 'Person not found' });
-    }
+    const query = 'SELECT * FROM personnes WHERE id = ?';
+    connection.query(query, [id], (error, results) => {
+        if (error) {
+            console.error('Erreur lors de la récupération de la personne :', error);
+            res.status(500).json({ error: 'Erreur serveur' });
+        }
+        else if (results === null || results.length === 0) {
+            res.status(404).json({ error: 'Personne non trouvée' });
+        }
+        else {
+            res.json(results[0]);
+        }
+    });
 });
 function writeDataToFile(data) {
     try {
-        const updatedData = data.map(person => (Object.assign(Object.assign({}, person), { id: person.id || (0, person_2.generateUniqueId)() // Génère un nouvel ID si l'ID est manquant ou non défini
-         })));
+        const updatedData = data.map(person => (Object.assign(Object.assign({}, person), { id: person.id || (0, person_1.generateUniqueId)() })));
         const jsonData = JSON.stringify(updatedData, null, 2);
         fs_1.default.writeFileSync('./dist/data.json', jsonData, 'utf8');
     }
@@ -50,35 +79,56 @@ function writeDataToFile(data) {
 }
 app.post('/people', (req, res) => {
     const { nom, prenom, mail, phone } = req.body;
-    const person = new person_1.Person(nom, prenom, mail, phone);
-    people.push(person);
-    writeDataToFile(people);
-    res.status(201).json(person);
+    const query = 'INSERT INTO personnes (nom, prenom, mail, phone) VALUES (?, ?, ?, ?)';
+    const values = [nom, prenom, mail, phone];
+    connection.query(query, values, (error, results) => {
+        if (error) {
+            console.error('Erreur lors de l\'ajout de la personne :', error);
+            res.status(500).json({ error: 'Erreur serveur' });
+        }
+        else {
+            const insertedPerson = { id: results.insertId, nom, prenom, mail, phone };
+            res.status(201).json(insertedPerson);
+        }
+    });
 });
 app.delete('/people/:id', (req, res) => {
     const id = req.params.id;
-    const index = people.findIndex(person => person.id === id);
-    if (index !== -1) {
-        const deletedPerson = people.splice(index, 1)[0];
-        writeDataToFile(people);
-        res.json(deletedPerson);
-    }
-    else {
-        res.status(404).json({ error: 'Person not found' });
-    }
+    const query = 'DELETE FROM personnes WHERE id = ?';
+    connection.query(query, [id], (error, results) => {
+        if (error) {
+            console.error('Erreur lors de la suppression de la personne :', error);
+            res.status(500).json({ error: 'Erreur serveur' });
+        }
+        else if (results.affectedRows === 0) {
+            res.status(404).json({ error: 'Personne non trouvée' });
+        }
+        else {
+            res.json({ message: 'Personne supprimée avec succès' });
+        }
+    });
 });
 app.put('/people', (req, res) => {
-    const updatedPerson = req.body;
-    const index = people.findIndex(person => person.id === updatedPerson.id);
-    if (index !== -1) {
-        people[index] = updatedPerson;
-        writeDataToFile(people);
-        res.json(updatedPerson);
-    }
-    else {
-        res.status(404).json({ error: 'Person not found' });
-    }
+    const { id, nom, prenom, mail, phone } = req.body;
+    const query = 'UPDATE personnes SET nom = ?, prenom = ?, mail = ?, phone = ? WHERE id = ?';
+    const values = [nom, prenom, mail, phone, id];
+    connection.query(query, values, (error, results) => {
+        if (error) {
+            console.error('Erreur lors de la mise à jour de la personne :', error);
+            res.status(500).json({ error: 'Erreur serveur' });
+        }
+        else if (results.affectedRows === 0) {
+            res.status(404).json({ error: 'Personne non trouvée' });
+        }
+        else {
+            res.json({ message: 'Personne mise à jour avec succès' });
+        }
+    });
 });
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
+});
+process.on('SIGINT', () => {
+    connection.end();
+    process.exit();
 });
--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,17 @@ import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import { Person } from './person';
 import { generateUniqueId } from './person';
+import { QueryError, RowDataPacket } from 'mysql2';
 import cors from 'cors';
 import fs from 'fs';
+import mysql from 'mysql2';
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
 const port = 3000;
+//const mysql = require('mysql2');
 
 function readDataFromFile() {
   try {
@@ -22,27 +25,57 @@ function readDataFromFile() {
   }
 }
 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'Camille',
+  password: 'linux_06',
+  database: 'my_db'
+});
+
 let people: Person[] = readDataFromFile();
 
+connection.connect((error: QueryError | null) => {
+  if (error) {
+    console.error('Erreur de connexion à la base de données :', error);
+  } else {
+    console.log('Connexion à la base de données réussie');
+  }
+});
+
+
 app.get('/people', (req: Request, res: Response) => {
-  res.json(people);
+  const query = 'SELECT * FROM personnes';
+  connection.query(query, (error: QueryError | null, results: RowDataPacket[] | null) => {
+    if (error) {
+      console.error('Erreur lors de la récupération des personnes :', error);
+      res.status(500).json({ error: 'Erreur serveur' });
+    } else {
+      res.json(results);
+    }
+  });
 });
 
 app.get('/people/:id', (req: Request, res: Response) => {
   const id = req.params.id;
-  const person = people.find(person => person.id === id);
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).json({ error: 'Person not found' });
-  }
+  const query = 'SELECT * FROM personnes WHERE id = ?';
+  connection.query(query, [id], (error: QueryError | null, results: RowDataPacket[]) => {
+    if (error) {
+      console.error('Erreur lors de la récupération de la personne :', error);
+      res.status(500).json({ error: 'Erreur serveur' });
+    } else if (results === null || results.length === 0) {
+      res.status(404).json({ error: 'Personne non trouvée' });
+    } else {
+      res.json(results[0]);
+    }
+  });
 });
+
 
 function writeDataToFile(data: Person[]): void {
   try {
     const updatedData = data.map(person => ({
       ...person,
-      id: person.id || generateUniqueId() // Génère un nouvel ID si l'ID est manquant ou non défini
+      id: person.id || generateUniqueId()
     }));
     const jsonData = JSON.stringify(updatedData, null, 2);
     fs.writeFileSync('./dist/data.json', jsonData, 'utf8');
@@ -51,43 +84,57 @@ function writeDataToFile(data: Person[]): void {
   }
 }
 
-
 app.post('/people', (req: Request, res: Response) => {
   const { nom, prenom, mail, phone } = req.body;
-  const person = new Person(nom, prenom, mail, phone);
-  people.push(person);
-  writeDataToFile(people);
-  res.status(201).json(person);
+  const query = 'INSERT INTO personnes (nom, prenom, mail, phone) VALUES (?, ?, ?, ?)';
+  const values = [nom, prenom, mail, phone];
+  connection.query(query, values, (error: QueryError | null, results: RowDataPacket[] & { insertId: number }) => {
+    if (error) {
+      console.error('Erreur lors de l\'ajout de la personne :', error);
+      res.status(500).json({ error: 'Erreur serveur' });
+    } else {
+      const insertedPerson = { id: results.insertId, nom, prenom, mail, phone };
+      res.status(201).json(insertedPerson);
+    }
+  });
 });
 
 app.delete('/people/:id', (req: Request, res: Response) => {
   const id = req.params.id;
-  const index = people.findIndex(person => person.id === id);
-  if (index !== -1) {
-    const deletedPerson = people.splice(index, 1)[0];
-    writeDataToFile(people);
-    res.json(deletedPerson);
-  } else {
-    res.status(404).json({ error: 'Person not found' });
-  }
+  const query = 'DELETE FROM personnes WHERE id = ?';
+  connection.query(query, [id], (error: QueryError | null, results: RowDataPacket[] & { affectedRows: number }) => {
+    if (error) {
+      console.error('Erreur lors de la suppression de la personne :', error);
+      res.status(500).json({ error: 'Erreur serveur' });
+    } else if (results.affectedRows === 0) {
+      res.status(404).json({ error: 'Personne non trouvée' });
+    } else {
+      res.json({ message: 'Personne supprimée avec succès' });
+    }
+  });
 });
 
 app.put('/people', (req: Request, res: Response) => {
-  const updatedPerson = req.body;
-  const index = people.findIndex(person => person.id === updatedPerson.id);
-  if (index !== -1) {
-    people[index] = updatedPerson;
-    writeDataToFile(people);
-    res.json(updatedPerson);
-  } else {
-    res.status(404).json({ error: 'Person not found' });
-  }
+  const { id, nom, prenom, mail, phone } = req.body;
+  const query = 'UPDATE personnes SET nom = ?, prenom = ?, mail = ?, phone = ? WHERE id = ?';
+  const values = [nom, prenom, mail, phone, id];
+  connection.query(query, values, (error: QueryError | null, results: RowDataPacket[] & { affectedRows: number }) => {
+    if (error) {
+      console.error('Erreur lors de la mise à jour de la personne :', error);
+      res.status(500).json({ error: 'Erreur serveur' });
+    } else if (results.affectedRows === 0) {
+      res.status(404).json({ error: 'Personne non trouvée' });
+    } else {
+      res.json({ message: 'Personne mise à jour avec succès' });
+    }
+  });
 });
-
-
-
-
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
+});
+
+process.on('SIGINT', () => {
+  connection.end();
+  process.exit();
 });
